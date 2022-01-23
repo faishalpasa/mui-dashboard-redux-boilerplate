26a031272053876e64a7bf0d6bafe987740a4d1e
Pasa <mfaishal_pasa@hotmail.com>
2022-01-23 22:28:09 +0700

    [ADD] AppBar in Layout

--- src/components/Layout/LayoutView.tsx
+++ src/components/Layout/LayoutView.tsx
@@ -1,13 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+import { Menu as MenuIcon, AccountCircle } from '@mui/icons-material'
 
 interface Layout {
   children: React.ReactNode
 }
 
-const LayoutView = ({ children }: Layout) => (
-  <div>
-    {children}
-  </div>
-)
+const LayoutView = ({ children }: Layout) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Photos
+          </Typography>
+          <Box>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+              onClick={handleMenu}
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {children}
+    </Box>
+  )
+}
 
 export default LayoutView
